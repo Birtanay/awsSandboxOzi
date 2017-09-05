aaa474ba3eca622240e36d9afd487047a17da966
Oguz Dag <oguz.dag@gmail.com>
2017-09-05 11:03:26 +0100

    Customised variable names

--- iamusers/iamusers.tf
+++ iamusers/iamusers.tf
@@ -1,23 +1,23 @@
-resource "aws_iam_group" "administrators" {
-    name = "administrators"
+resource "aws_iam_group" "oziadministrators" {
+    name = "my-administrator-group"
 }
-resource "aws_iam_policy_attachment" "administrators-attach" {
-    name = "administrators-attach"
-    groups = ["${aws_iam_group.administrators.name}"]
+resource "aws_iam_policy_attachment" "administrators-policy-attach" {
+    name = "ozi-administrators-policy-attach"
+    groups = ["${aws_iam_group.oziadministrators.name}"]
     policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
 }
 # user
-resource "aws_iam_user" "admin1" {
-    name = "admin1"
+resource "aws_iam_user" "oziuser1" {
+    name = "OziUser1"
 }
-resource "aws_iam_user" "admin2" {
-    name = "admin2"
+resource "aws_iam_user" "oziuser2" {
+    name = "OziUser2"
 }
 resource "aws_iam_group_membership" "administrators-users" {
     name = "administrators-users"
     users = [
-        "${aws_iam_user.admin1.name}",
-        "${aws_iam_user.admin2.name}",
+        "${aws_iam_user.oziuser1.name}",
+        "${aws_iam_user.oziuser2.name}",
     ]
-    group = "${aws_iam_group.administrators.name}"
+    group = "${aws_iam_group.oziadministrators.name}"
 }
